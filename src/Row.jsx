da30a82393452449187d9ebd7f09f8ae7c8944be
Tesla <bolarinwaahmed22@gmail.com>
2021-12-06 19:40:39 +0100

    Made some modifications in nav and row component

--- src/Row.jsx
+++ src/Row.jsx
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from "react";
 import instance from "./axios";
 import "./Row.css";
+import Youtube from "react-youtube";
+import movieTrailer from "movie-trailer";
 
 function Row({ title, fetchUrl, isLargeRow }) {
   const imgBaseUrl = "http://image.tmdb.org/t/p/original";
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
   useEffect(() => {
     const fetchData = async () => {
@@ -13,7 +16,41 @@ function Row({ title, fetchUrl, isLargeRow }) {
     };
     fetchData();
   }, [fetchUrl]);
-  //   console.log(movies);
+
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+      origin: `${window.location.origin}`,
+      host: `${window.location.protocol}`,
+    },
+  };
+
+  //  _onReady(event) {
+  //     // access to player in all event handlers via event.target
+  //     event.target.pauseVideo();
+  //   };
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.name || movie?.title || movie?.original_name || "")
+        .then((url) => {
+          console.log(url);
+          console.log(movie?.name);
+          const urlLink = new URL(url);
+          const urlSearch = urlLink.search;
+          const urlParams = new URLSearchParams(urlSearch);
+          setTrailerUrl(JSON.stringify(urlParams.get("v")));
+          console.log(trailerUrl);
+        })
+        .catch((error) => console.log(error));
+    }
+  };
+
   return (
     <div className="row">
       <h2>{title}</h2>
@@ -21,6 +58,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
         {movies
           ? movies.map((movie) => (
               <img
+                onClick={() => handleClick(movie)}
                 key={movie.id}
                 src={`${imgBaseUrl}${
                   isLargeRow ? movie.poster_path : movie.backdrop_path
@@ -31,6 +69,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
             ))
           : "No movies to render"}
       </div>
+      {trailerUrl && <Youtube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 }
